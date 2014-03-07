f115ba17a937ba75cc0dbbd4db89479b074f135b
lhshin <379236029@qq.com>
2014-03-06 22:38:22 -0800

    test

--- NewZhongYan/NewZhongYan/SKAppDelegate.h
+++ NewZhongYan/NewZhongYan/SKAppDelegate.h
@@ -11,7 +11,6 @@
 @class SKAgentLogonManager;
 @class User;
 #define System_Version_Small_Than_(v) (DeviceSystemMajorVersion() < v)
-///1111
 @interface SKAppDelegate : UIResponder <UIApplicationDelegate>
 @property (strong, nonatomic) UIWindow *window;
 @property (strong, nonatomic) UIStoryboard *mainStoryboard;
